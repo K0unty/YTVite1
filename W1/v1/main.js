07eefd0f76762a9d405a22ae4a779d7c973ea7eb
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-19 02:44:14 +0000

    .

--- W1/v1/main.js
+++ W1/v1/main.js
@@ -1,9 +1,4 @@
 /**
  * Original vite main.js removed now doing work from the tutorial here
  */
-import "./src/03.css";
-import stylesA from "./src/assets/04a.module.css";
-import stylesB from "./src/assets/04b.module.css";
-
-console.log(stylesA);
-console.log(stylesB);
+import "./src/assets/05.css";
