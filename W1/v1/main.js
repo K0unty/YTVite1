8ed2bde6d0d07f72c12582e4008c98c49d81416d
K0unty <119057948+K0unty@users.noreply.github.com>
2024-06-19 08:10:59 +0000

    .

--- W1/v1/main.js
+++ W1/v1/main.js
@@ -1,6 +1,7 @@
 /**
  * Original vite main.js removed now doing work from the tutorial here
  */
-import styles from "./src/assets/06.module.sass";
+// import styles from "./src/assets/06.module.sass";
+import data from "./src/assets/07.json";
 
 console.log(styles);
